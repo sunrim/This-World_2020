5a7b878c405a2622b0120b00b42fc900472dc8e2
김선옥 <sictami1012@gmailc.om>
2020-05-25 16:11:01 +0900

    left-right moving
    character's left right moving

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+    public float speed;
+    
+    private Vector3 vector;
+
+    public float runSpeed;
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount;
+    private int currentWalkCount;
+
+    private bool canMove = true;
+
+    void Start() {
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+        else
+        {
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+        }
+
+
+        vector.Set(Input.GetAxisRaw("Horizontal"), transform.position.y, transform.position.z);
+
+        while (currentWalkCount < walkCount)
+        {
+            if (vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            }
+
+            if (applyRunFlag)
+            {
+                currentWalkCount++;
+            }
+
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        currentWalkCount = 0;
+        canMove = true;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+        
+    }
+}
