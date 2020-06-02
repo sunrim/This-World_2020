067a5c1a17a51b5f54a8294fef0168801ee5d4e0
unknown <123456guswls@naver.com>
2020-06-02 22:21:58 +0900

    디비,아이템,인벤(중간)

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -25,9 +25,18 @@ public class DatabaseManager : MonoBehaviour
     public string[] switch_name;
     public bool[] switches;
 
+    public List<Item> itemList = new List<Item>();
+
     // Start is called before the first frame update
     void Start()
     {
-
+        itemList.Add(new Item(00001, "망치", "무언가를 부실 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00002, "교무실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use)):
+        itemList.Add(new Item(00003, "과학실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00004, "보건실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00005, "실습실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00006, "조교실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00007, "기사실 열쇠", "잠긴 곳을 열 수 있을 것 같다.",  Item.ItemType.Use));
+        itemList.Add(new Item(00008, "의약품", "모든지 치료할 수 있을 것 같다.",  Item.ItemType.Use))
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item{
+
+    public int itemID;//아이템의 고유ID 중복 불가능
+    public string itemName; // 아이템 이름 중복가능
+    public string itemDescription;
+    public int itemCount;
+    public Sprite itemIcon;
+    public ItemType itemType;
+
+    public enum ItemType{
+        Use,
+        Equip,
+        Quest,
+        ETC
+    }
+
+    public Item(int _itemID, string _itemName, string _itemDes, ItemType _itemType, int _itemCount=1){
+        itemID = _itemID;
+        itemName = _itemName;
+        itemDescription = _itemDes;
+        itemType =_itemType;
+        itemCount = _intCount;
+        itemIcon = Resources.Load("ItemIcon/" + _itemID.ToString(), typeof(Sprite))as Sprite;
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
